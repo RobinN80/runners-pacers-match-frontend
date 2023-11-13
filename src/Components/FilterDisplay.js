356c6ec0e95726d958f20a3d8f42bdded45edec2
Robin Novak <robin@translator.company>
2023-11-13 15:44:19 -0800

    Begin creating filter

--- src/Components/FilterDisplay.js
+++ src/Components/FilterDisplay.js
@@ -1,7 +1,31 @@
-const FilterDisplay = () =>{
-    return(
-        <div></div>
-    );
-}
+import React, {useState} from 'react';
+import {FormControl, FormLabel, Select, MenuItem, Grid, Box} from '@mui/material';
+// import '../CompnentCSS/FilterDisplay.css'
 
-export default FilterDisplay;
+const Example = () => { 
+    // const [test] = useState(true); 
+    return ( 
+        <Box component="section" id="box">
+            <Grid container spacing={0}>
+                <Grid item xs={12} id="item">
+                <FormControl variant="filled">
+                    <FormLabel id="filter-by-gender">Gender</FormLabel>
+                    <Select
+                        labelId="filter-by-gender"
+                        value=""
+                    >
+                        <MenuItem value="male">Male</MenuItem>
+                        <MenuItem value="female">Female</MenuItem>
+                    </Select>
+                    
+                </FormControl>
+                </Grid>
+            </Grid>
+            
+
+        </Box>
+    ); 
+} 
+export default Example;
+
+// export default FilterDisplay;
