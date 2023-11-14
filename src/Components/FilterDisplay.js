17a2e26a8f8a1976a08bf50a8406805df7c5bbc9
Robin Novak <robin@translator.company>
2023-11-13 16:42:29 -0800

    Update FilterDisplay.js
    add select distance

--- src/Components/FilterDisplay.js
+++ src/Components/FilterDisplay.js
@@ -2,11 +2,16 @@ import React, {useState} from 'react';
 import {FormControl, FormLabel, Select, MenuItem, Grid, Box} from '@mui/material';
 // import '../CompnentCSS/FilterDisplay.css'
 import SelectGender from './Forms/SelectGender';
+import SelectDistance from './Forms/SelectDistance';
 
 const Example = () => { 
     // const [test] = useState(true); 
     const handleChangeGender = (event) => {
-        console.log(event.target.value, 'inside filter');
+        console.log(event.target.value, 'inside filter gender');
+    }
+
+    const handleChangeDistance = (event) => {
+        console.log(event.target.value, "filter distance")
     }
     return ( 
         <Box component="section" id="box">
@@ -14,11 +19,19 @@ const Example = () => {
                 <Grid item xs={12} id="item">
                 <FormControl variant="filled">
                     <FormLabel id="filter-by-gender">Gender</FormLabel>
-                        <SelectGender lableId="filter-by-gender" gender="gender placeholder"
+                        <SelectGender lableId="filter-by-gender" gender="gender PLACEHOLDER"
                         handleChangeGender={() => handleChangeGender}
                     
                     />
-                    
+                </FormControl>
+                <FormControl>
+                    <FormLabel id="Select Distance">
+                        Distance
+                    </FormLabel>
+                    <SelectDistance 
+                        distance="distance PLACEHOLDER"
+                        handleChangeDistance={()=> handleChangeDistance}
+                    />
                 </FormControl>
                 </Grid>
             </Grid>
